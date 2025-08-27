295394afc85fb84df9da48bf793113f447809828
hanifmz07 <13521157@std.stei.itb.ac.id>
2025-08-27 00:43:33 +0000

    feat: add code for uploading results

---
+++ run_upload_results.py
@@ -0,0 +1,35 @@
+import os
+import glob
+from huggingface_hub import HfApi
+from huggingface_hub import login
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+# Login HuggingFace
+login(token=os.getenv("HF_TOKEN"))
+api = HfApi()
+
+# Define your repository details, the outputs parent directory, task, and prompt configuration
+repo_id = "indolinguafrancaresearch/extracted-activations"  
+parent_directory = "outputs"     
+task = 'machine_translation'
+prompt_config = 'prompt_en'
+
+# Get all compressed experiment results (activations)
+files_to_upload = glob.glob(os.path.join(parent_directory, task, '*', prompt_config, '*.tar.gz'))
+files_to_upload
+
+# Loop through and upload each file
+for file_path in files_to_upload:
+    if os.path.isfile(file_path):
+        print(f"Uploading {file_path}...")
+        api.upload_file(
+            path_or_fileobj=file_path,
+            repo_id=repo_id,
+            repo_type="dataset",
+            path_in_repo=file_path,  # Creates a folder with the same name in the repo
+        )
+        print(f"Successfully uploaded {file_path}.")
+
+print("All specified files have been uploaded.")
