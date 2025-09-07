228f9d6aebdaf9021dfbc81e30635d0bc3013dd7
Tansil011019 <wilsontansil@gmail.com>
2025-09-07 18:42:30 +0700

    feat: add generate silhotte score plot

--- src/utils/activation_visualizer.py
+++ src/utils/activation_visualizer.py
@@ -103,16 +103,76 @@ class ActivationVisualizer:
             plt.savefig(f"{save_path}/{self.models[model_id]['name']}_{plot_by}_{input_mode}_{extraction_mode}.{ext}", bbox_inches='tight')
             plt.show()
 
-    def generate_silhoutte_score(self):
+    def generate_silhoutte_score(
+            self, 
+            save_path: str = "results",
+            ext: Literal["png", "jpg", "jpeg", "pdf"] = "pdf",
+            activation_path: str = "./activations/extracted",
+            input_mode: Literal["raw", "prompted"] = "raw",
+            extraction_mode: Literal["last_token", "average", "first_token"] = "average",
+        ):
         num_models = len(self.models)
-        fig, axes = plt.subplots(1, num_models, figsize=(10 * num_models, 6))
+        fig, axes = plt.subplots(2, num_models//2, figsize=(5 * (num_models//2), 10))
+        axes = axes.flatten()
 
-        language_scores = []
-        topic_scores = []
-        text_id_scores = []
+        for model_id in range(len(self.models)):
+            language_scores = []
+            topic_scores = []
+            text_id_scores = []
 
-        # for layer in range (-1, )
+            for layer in tqdm(range(-1, self.models[model_id]['num_layers']), desc = f"Processing Model {self.models[model_id]['name']} Layers"):
+                language_labels = []
+                topic_labels = []
+                text_id_labels = []
+                latent = []
 
+                for current_language in self.languages:
+                    if activation_path is None:
+                        raise ValueError("activation_path must be provided")
+                    base_path = os.path.join(activation_path, self.models[model_id]['name'], input_mode, current_language, current_language)
+                    for text_id in os.listdir(base_path):
+                        text_path = os.path.join(base_path, text_id)
+                        if not os.path.isdir(text_path):
+                            continue
+                        path = os.path.join(text_path, extraction_mode, f"layer_{"embed_tokens" if layer == -1 else layer}.pt")
+                        if not os.path.exists(path):
+                            print(f"Warning: File {path} does not exist, skipping...")
+                            continue
+                        try:
+                            activation_values = torch.load(path)
+                        except EOFError:
+                            print(f"Error loading {path}, skipping...")
+                            continue
+                        latent.append(activation_values.to(torch.float32).numpy())
+                        current_data = self.data[current_language]
+                        matching_row = current_data[current_data['index_id'].astype(str) == text_id]
+                        if matching_row.empty:
+                            raise ValueError(f"Warning: No matching data found for text_id {text_id} in language {current_language}")
+                        topic_labels.append(matching_row['category'].values[0])
+                        language_labels.append(current_language)
+                        text_id_labels.append(text_id)
+
+                latent = np.array(latent)
+                language_score = silhouette_score(latent, language_labels, metric="euclidean")
+                topic_score = silhouette_score(latent, topic_labels, metric="euclidean")
+                text_id_score = silhouette_score(latent, text_id_labels, metric="euclidean")
+
+                language_scores.append(language_score)
+                topic_scores.append(topic_score)
+                text_id_scores.append(text_id_score)
+        
+            axes[model_id].plot(language_scores, label="Language")
+            axes[model_id].plot(topic_scores, label="Topic")
+            axes[model_id].plot(text_id_scores, label="Text ID")
+            axes[model_id].set_title(self.models[model_id]['name'])
+            axes[model_id].set_xlabel("Layer")
+            axes[model_id].set_ylabel("Silhouette Score")
+            axes[model_id].legend()
+
+        plt.tight_layout()
+        plt.savefig(f"{save_path}/silhoutte_score_{input_mode}_{extraction_mode}.{ext}", bbox_inches='tight')
+        plt.show()
+                    
     # TODO: Machine Translation plotting    
     def generate_plots_machine_translation(
             self, 
