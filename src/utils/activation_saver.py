0f48c077844cdde528515a0adc236f06698efe23
hanifmz07 <13521157@std.stei.itb.ac.id>
2025-11-06 15:20:32 +0000

    feat: add implementation to save activations after attention block (residual after post attn layer norm)

--- src/utils/activation_saver.py
+++ src/utils/activation_saver.py
@@ -2,56 +2,80 @@ import os
 import torch
 
 class ActivationSaver:
-    def __init__(self, base_save_dir: str, task_id: str, model_name: str, prompt_id: str):
-        self.task_id = task_id
-        self.model_name = model_name
-        self.prompt_id = prompt_id
-        self.base_save_dir = base_save_dir
-        self.current_id = None
-        self.current_lang = "en"
+	def __init__(self, base_save_dir: str, task_id: str, model_name: str, prompt_id: str):
+		self.task_id = task_id
+		self.model_name = model_name
+		self.prompt_id = prompt_id
+		self.base_save_dir = base_save_dir
+		self.current_id = None
+		self.current_lang = "en"
 
-    def set_id(self, new_id):
-        self.current_id = new_id
+	def set_id(self, new_id):
+		self.current_id = new_id
 
-    def set_lang(self, new_lang):
-        self.current_lang = new_lang
+	def set_lang(self, new_lang):
+		self.current_lang = new_lang
 
-    def hook_fn(self, module, input, output, layer_id):
-        if self.current_id is None:
-            print(f"Warning: ID not set for layer {layer_id}")
-            return
+	def hook_fn(self, module, input, output, layer_id):
+		if self.current_id is None:
+			print(f"Warning: ID not set for layer {layer_id}")
+			return
 
-        if self.current_lang is None:
-            print(f"Warning: Language not set for layer {layer_id}")
-            return
+		if self.current_lang is None:
+			print(f"Warning: Language not set for layer {layer_id}")
+			return
 
-        if any(list(map(lambda x: x in self.model_name.lower(), ['gemma']))) and layer_id not in ['embed_tokens', 'norm']:
-            path = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "last_token")
-            os.makedirs(path, exist_ok=True)
-            save_path = os.path.join(path, f"layer_{layer_id}.pt")
-            torch.save(output[0][0, -1, :].detach().cpu(), save_path) 
+		if any(list(map(lambda x: x in self.model_name.lower(), ['gemma']))) and layer_id not in ['embed_tokens', 'norm']:
+			path = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "last_token")
+			os.makedirs(path, exist_ok=True)
+			save_path = os.path.join(path, f"layer_{layer_id}.pt")
+			torch.save(output[0][0, -1, :].detach().cpu(), save_path) 
 
-            path = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "average")
-            os.makedirs(path, exist_ok=True)
-            save_path = os.path.join(path, f"layer_{layer_id}.pt")
-            torch.save(output[0][0].mean(dim=0).detach().cpu(), save_path) 
+			path = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "average")
+			os.makedirs(path, exist_ok=True)
+			save_path = os.path.join(path, f"layer_{layer_id}.pt")
+			torch.save(output[0][0].mean(dim=0).detach().cpu(), save_path) 
 
-        # Extraction for any other layers/models
-        else:
-            path = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "last_token")
-            os.makedirs(path, exist_ok=True)
-            save_path = os.path.join(path, f"layer_{layer_id}.pt")
-            torch.save(output[0, -1, :].detach().cpu(), save_path) 
+		# Extraction for any other layers/models
+		else:
+			path = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "last_token")
+			os.makedirs(path, exist_ok=True)
+			save_path = os.path.join(path, f"layer_{layer_id}.pt")
+			torch.save(output[0, -1, :].detach().cpu(), save_path) 
 
-            path = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "average")
-            os.makedirs(path, exist_ok=True)
-            save_path = os.path.join(path, f"layer_{layer_id}.pt")
-            torch.save(output[0].mean(dim=0).detach().cpu(), save_path) 
+			path = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "average")
+			os.makedirs(path, exist_ok=True)
+			save_path = os.path.join(path, f"layer_{layer_id}.pt")
+			torch.save(output[0].mean(dim=0).detach().cpu(), save_path)
+	
+	def pre_hook_fn(self, module, input, layer_id):
+		if self.current_id is None:
+			print(f"Warning: ID not set for layer {layer_id}")
+			return
 
-    # Check if activations for an instance already exist
-    def check_exists(self):
-        path_last_token = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "last_token")
-        path_average = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "average")
-        check_files = os.listdir(path_last_token) if os.path.exists(path_last_token) else []
-        check_files += os.listdir(path_average) if os.path.exists(path_average) else []
-        return bool(check_files)
+		if self.current_lang is None:
+			print(f"Warning: Language not set for layer {layer_id}")
+			return
+
+		# Extract residual connection after attention (precisely after post-attention layer norm)
+		save_path = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "last_token")
+		os.makedirs(save_path, exist_ok=True)
+		save_path = os.path.join(save_path, f"layer_{layer_id}.pt")
+		torch.save(input[0][0, -1, :].detach().cpu(), save_path)
+
+	# Check if activations for an instance already exist
+	def check_exists(self):
+		path_last_token = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "last_token")
+		path_average = os.path.join(self.base_save_dir, self.task_id, self.model_name.split('/')[-1], self.prompt_id, self.current_lang, self.current_id, "average")
+		check_files = os.listdir(path_last_token) if os.path.exists(path_last_token) else []
+
+		# Check if post-attention norm files exist as well
+		post_attn_files = [f for f in check_files if 'postattn-norm' in f]
+		if len(post_attn_files) == 0:
+			return False
+		
+		# Check average directory files
+		check_files += os.listdir(path_average) if os.path.exists(path_average) else []
+
+		# If there are any files, return True
+		return bool(check_files)
--- src/utils/hooked_model.py
+++ src/utils/hooked_model.py
@@ -7,68 +7,72 @@ import os
 load_dotenv() 
 
 class HookedModel:
-    def __init__(self, model_name: str, saver: ActivationSaver):
+	def __init__(self, model_name: str, saver: ActivationSaver):
 
-        device = "cpu"
-        model_dtype = torch.float16
-        if torch.cuda.is_available():
-            device = "cuda"
-            compute_capability = torch.cuda.get_device_capability()[0]
+		device = "cpu"
+		model_dtype = torch.float16
+		if torch.cuda.is_available():
+			device = "cuda"
+			compute_capability = torch.cuda.get_device_capability()[0]
 
-            # Use bfloat16 if supported
-            if compute_capability >= 8:
-                model_dtype = torch.bfloat16
-        
-        self.model_name = model_name
-        self.saver = saver
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=model_dtype, device_map=device, cache_dir=os.getenv("HF_CACHE_DIR"))
-        self.model.eval()
-        self._setup_hooks()
+			# Use bfloat16 if supported
+			if compute_capability >= 8:
+				model_dtype = torch.bfloat16
+		
+		self.model_name = model_name
+		self.saver = saver
+		self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=model_dtype, device_map=device, cache_dir=os.getenv("HF_CACHE_DIR"))
+		self.model.eval()
+		self._setup_hooks()
 
-    def _setup_hooks(self):
-        if 'bloom' in self.model_name:
-            for i, layer in enumerate(self.model.transformer.h):
-                layer.register_forward_hook(lambda module, input, output, layer_id=i: self.saver.hook_fn(module, input, output, layer_id))
-        else:
-            multimodal_models = ['gemma-3-4b', 'gemma-3-12b', 'gemma-3-27b']
-            if any(model in self.model_name.lower() for model in multimodal_models):
-                self.model.model.language_model.embed_tokens.register_forward_hook(lambda module, input, output, layer_id="embed_tokens": self.saver.hook_fn(module, input, output, layer_id))
-                self.model.model.language_model.norm.register_forward_hook(lambda module, input, output, layer_id="norm": self.saver.hook_fn(module, input, output, layer_id))
-                for i, layer in enumerate(self.model.model.language_model.layers):
-                    layer.register_forward_hook(lambda module, input, output, layer_id=i: self.saver.hook_fn(module, input, output, layer_id))
-            else:
-                self.model.model.embed_tokens.register_forward_hook(lambda module, input, output, layer_id="embed_tokens": self.saver.hook_fn(module, input, output, layer_id))
-                self.model.model.norm.register_forward_hook(lambda module, input, output, layer_id="norm": self.saver.hook_fn(module, input, output, layer_id))
-                for i, layer in enumerate(self.model.model.layers):
-                    layer.register_forward_hook(lambda module, input, output, layer_id=i: self.saver.hook_fn(module, input, output, layer_id))
-            
-            # TODO: Figure out how to extract components of the model that have been applied with RoPE
-            # self.model.model.rotary_emb.register_forward_hook(lambda module, input, output, layer_id="rotary_emb": self.saver.hook_fn(module, input, output, layer_id))
-        
+    # TODO: Check compatibility for non-gemma3 models
+	def _setup_hooks(self):
+		if 'bloom' in self.model_name:
+			for i, layer in enumerate(self.model.transformer.h):
+				layer.register_forward_hook(lambda module, input, output, layer_id=i: self.saver.hook_fn(module, input, output, layer_id))
+		else:
+			multimodal_models = ['gemma-3-4b', 'gemma-3-12b', 'gemma-3-27b']
+			if any(model in self.model_name.lower() for model in multimodal_models):
+				self.model.model.language_model.embed_tokens.register_forward_hook(lambda module, input, output, layer_id="embed_tokens": self.saver.hook_fn(module, input, output, layer_id))
+				self.model.model.language_model.norm.register_forward_hook(lambda module, input, output, layer_id="norm": self.saver.hook_fn(module, input, output, layer_id))
+				for i, layer in enumerate(self.model.model.language_model.layers):
+					layer.register_forward_hook(lambda module, input, output, layer_id=i: self.saver.hook_fn(module, input, output, layer_id))
+					layer.post_attention_layernorm.register_forward_pre_hook(lambda module, input, layer_id=f"postattn-norm_{i}": self.saver.pre_hook_fn(module, input, layer_id))
+			else:
+				self.model.model.embed_tokens.register_forward_hook(lambda module, input, output, layer_id="embed_tokens": self.saver.hook_fn(module, input, output, layer_id))
+				self.model.model.norm.register_forward_hook(lambda module, input, output, layer_id="norm": self.saver.hook_fn(module, input, output, layer_id))
+				for i, layer in enumerate(self.model.model.layers):
+					layer.register_forward_hook(lambda module, input, output, layer_id=i: self.saver.hook_fn(module, input, output, layer_id))
+					layer.post_attention_layernorm.register_forward_pre_hook(lambda module, input, layer_id=f"postattn-norm_{i}": self.saver.pre_hook_fn(module, input, layer_id))
 
-    def set_saver_id(self, new_id: int):
-        self.saver.set_id(new_id)
+			# TODO: Figure out how to extract components of the model that have been applied with RoPE
+			# self.model.model.rotary_emb.register_forward_hook(lambda module, input, output, layer_id="rotary_emb": self.saver.hook_fn(module, input, output, layer_id))
+		
 
-    def set_saver_lang(self, new_lang: str):
-        self.saver.set_lang(new_lang)
-        
-    def generate(self, inputs):
-        with torch.no_grad():
-            outputs = self.model.generate(
-                **inputs,
-                max_new_tokens=1,
-            )
-        return outputs
+	def set_saver_id(self, new_id: int):
+		self.saver.set_id(new_id)
 
-    # Clear hooks for debugging purposes
-    def clear_hooks(self):
-        if 'bloom' in self.model_name:
-            for i, layer in enumerate(self.model.transformer.h):
-                layer._forward_hooks.clear()
-        else:
-            self.model.model.embed_tokens._forward_hooks.clear()
-            self.model.model.norm._forward_hooks.clear()
-            for i, layer in enumerate(self.model.model.layers):
-                layer._forward_hooks.clear()
+	def set_saver_lang(self, new_lang: str):
+		self.saver.set_lang(new_lang)
+		
+	def generate(self, inputs):
+		with torch.no_grad():
+			outputs = self.model.generate(
+				**inputs,
+				max_new_tokens=1,
+			)
+		return outputs
 
-            # self.model.model.rotary_emb._forward_hooks.clear()
+	# Clear hooks for debugging purposes
+	def clear_hooks(self):
+		if 'bloom' in self.model_name:
+			for i, layer in enumerate(self.model.transformer.h):
+				layer._forward_hooks.clear()
+		else:
+			self.model.model.embed_tokens._forward_hooks.clear()
+			self.model.model.norm._forward_hooks.clear()
+			for i, layer in enumerate(self.model.model.layers):
+				layer._forward_hooks.clear()
+				layer._forward_pre_hooks.clear()
+
+			# self.model.model.rotary_emb._forward_hooks.clear()
